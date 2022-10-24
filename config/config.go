5589cfd3e7fdcbd0927e315100dc445990521520
dominikusvieri <dominikusvieri9@gmail.com>
2022-10-24 20:13:27 +0700

    pertamax

---
+++ config/config.go
@@ -0,0 +1,10 @@
+package config
+
+const (
+	DB_HOST     = "localhost"
+	DB_PORT     = 5432
+	DB_USER     = "postgres"
+	DB_PASSWORD = "admin"
+	DB_NAME     = "final"
+	APP_PORT    = ":8888"
+)
---
+++ config/db.go
@@ -0,0 +1,21 @@
+package config
+
+import (
+	"final-project/models"
+	"fmt"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func ConnectDB() *gorm.DB {
+	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname =%s sslmode=disable", DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic(err)
+	}
+
+	db.AutoMigrate(models.User{}, models.Photo{}, models.Comment{}, models.SocialMedia{})
+	return db
+}
